32ee6d925c9a710bc8464f197a71ba43ce7e8a17
alessio <filippini.alessio@gmail.com>
2014-11-20 16:01:26 +0100

    Correzione.

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <?php 
 if ($_GET['file'] != '' && file_exists('images/' . str_replace('..', '', $_GET['file']))) {
-	header("Content-disposition: attachment; filename=" . basename($_GET['file']));
-	header("Content-type: application/pdf");
+ 	header("Content-disposition: attachment; filename=" . basename($_GET['file']));
+ 	header("Content-type: application/pdf");
 	
-	readfile('/images/' . str_replace('..', '', $_GET['file']));
+	readfile('images/' . str_replace('..', '', $_GET['file']));
 }
