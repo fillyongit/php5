e37c55cbfec6a3839850a8c5f316d523a479728f
alessio <filippini.alessio@gmail.com>
2014-11-20 15:15:52 +0100

    download

---
+++ download.php
@@ -0,0 +1,7 @@
+<?php 
+if ($_GET['file'] != '' && file_exists('images/' . str_replace('..', '', $_GET['file']))) {
+	header("Content-disposition: attachment; filename=" . basename($_GET['file']));
+	header("Content-type: application/pdf");
+	
+	readfile('/images/' . str_replace('..', '', $_GET['file']));
+}
